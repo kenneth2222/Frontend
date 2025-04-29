ed31b66257f15c9a723eec47d3a5f18b800a9999
Jedidiah Solomon <121108148+Jedidiah-Solomon@users.noreply.github.com>
2025-04-29 08:19:57 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
+import SponsorSignup from "./components/SponsorSignup";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SponsorSignup />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
